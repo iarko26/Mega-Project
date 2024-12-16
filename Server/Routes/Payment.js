ea2427ecc09117199775925e90b6febbc30c5094
Arnob-Ghosh <94540571+iarko26@users.noreply.github.com>
2024-12-16 15:47:08 +0600

    Delete Server directory

--- Server/Routes/Payment.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {auth,isStudent,isAdmin,isInstructor}=require('../Middleware/auth');
-
-const {capturePayment,verifysignature}=require('../Controller/Payment');
-router.post('/capturePayment',auth,isStudent,capturePayment);
-router.post('/verifySignature',verifysignature);
-module.exports=router;
